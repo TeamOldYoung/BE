9d81b469def1059a18d05e0aeac3b5d87c1b711f
Neo1228 <4987kk@naver.com>
2025-08-02 17:28:52 +0900

    [SETTING] 초기 세팅

---
+++ oldYoung/src/main/java/com/app/oldYoung/OldYoungApplication.java
@@ -0,0 +1,13 @@
+package com.app.oldYoung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldYoungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldYoungApplication.class, args);
+	}
+
+}
---
+++ oldYoung/src/test/java/com/app/oldYoung/OldYoungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.oldYoung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldYoungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
