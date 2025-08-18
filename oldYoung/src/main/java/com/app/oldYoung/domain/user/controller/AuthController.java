4f0f0fde2b28421b2397626c28fa027c703b7e4c
margie1a <4987kk@naver.com>
2025-08-19 00:25:02 +0900

    [FEAT] OAuth2 API 구현 및 yml 환경변수, 의존성 추가

--- oldYoung/src/main/java/com/app/oldYoung/domain/user/controller/AuthController.java
+++ oldYoung/src/main/java/com/app/oldYoung/domain/user/controller/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("")
+@RequestMapping("/api")
 public class AuthController {
 
     private final AuthService authService;
