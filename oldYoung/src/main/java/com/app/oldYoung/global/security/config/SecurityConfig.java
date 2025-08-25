1f4d49bd1d4ef609941e38cc8b2ff78978acef22
Dobby19 <southvi1@naver.com>
2025-08-25 12:02:20 +0900

    [FEAT] 배포주소 CORS 허용

--- oldYoung/src/main/java/com/app/oldYoung/global/security/config/SecurityConfig.java
+++ oldYoung/src/main/java/com/app/oldYoung/global/security/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080", "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080", "http://localhost:5173","http://54.180.208.86"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
