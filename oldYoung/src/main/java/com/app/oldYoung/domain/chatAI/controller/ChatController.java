83289e5f5aba5ef31f8608bfbe17d48086e5766d
Dobby19 <southvi1@naver.com>
2025-08-26 22:04:04 +0900

    [REFACTOR] api 경로 수정

--- oldYoung/src/main/java/com/app/oldYoung/domain/chatAI/controller/ChatController.java
+++ oldYoung/src/main/java/com/app/oldYoung/domain/chatAI/controller/ChatController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/harume")
+@RequestMapping("/api/harume")
 @RequiredArgsConstructor
 @Tag(name = "Chat API", description = "AI 채팅 서비스 API")
 public class ChatController {
