6adcf14df3fefd8f0e5da3bd837d8f8983d6f3c1
margie1a <4987kk@naver.com>
2025-08-26 21:45:49 +0900

    [REFACTOR] DTO 수정

--- oldYoung/src/main/java/com/app/oldYoung/domain/ai/dto/WelfareRequestDTO.java
+++ oldYoung/src/main/java/com/app/oldYoung/domain/ai/dto/WelfareRequestDTO.java
@@ -1,8 +1,12 @@
 package com.app.oldYoung.domain.ai.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record WelfareRequestDTO(
+        @JsonProperty("age(bool)")
         int age,
         
+        @JsonProperty("city")
         String region
 ) {
 }
