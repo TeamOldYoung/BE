afb46319721cd045b9339fb57cf35c2574782ed6
Dobby19 <southvi1@naver.com>
2025-08-26 18:30:41 +0900

    [REFACTOR] CORS 설정 허용

--- oldYoung/src/main/java/com/app/oldYoung/global/security/config/SecurityConfig.java
+++ oldYoung/src/main/java/com/app/oldYoung/global/security/config/SecurityConfig.java
@@ -62,10 +62,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(false);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
